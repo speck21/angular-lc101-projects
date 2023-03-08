93e054ee2b5593aea8b7dd73deaaa928233907b5
Speck <stephan.speck21@gmail.com>
2023-03-08 02:55:18 -0600

    29.7.2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image1 = 'https://imgs.search.brave.com/2EF90i95mAMnwYpK2zOXTFxdMNgYM-V97rGtar-ZTkg/rs:fit:592:612:1/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vdmVjdG9y/cy9jYXJ0b29uLWFy/Y2hlcnktdGFyZ2V0/LXZlY3Rvci1pZDE0/ODc3MTczOT9rPTYm/bT0xNDg3NzE3Mzkm/cz02MTJ4NjEyJnc9/MCZoPWdFc2h5dlVq/OGRqRXpORGFPejNt/Vks5VDRqSXNROUJ1/ZWh2eFFiU2hndEE9';
+  image2 = 'https://imgs.search.brave.com/8_7Ozj0gstDXoV6O18WfZut9pfc892EntinFg1_9drY/rs:fit:1000:1200:1/g:ce/aHR0cHM6Ly9waWNm/aWxlcy5hbHBoYWNv/ZGVycy5jb20vMTQw/LzE0MDEwMy5qcGc';
+  image3 = 'https://imgs.search.brave.com/cZxqKBxoeB6RFhy7i-vZbrpQNPvZGrezIfZEq2vVkzs/rs:fit:1200:1200:1/g:ce/aHR0cDovL25hdGlv/bmFsaW50ZXJlc3Qu/b3JnL3NpdGVzL2Rl/ZmF1bHQvZmlsZXMv/bWFpbl9pbWFnZXMv/c3dlZ2Rlc2dzZGdk/Zi5KUEVH';
 
   constructor() { }
 
